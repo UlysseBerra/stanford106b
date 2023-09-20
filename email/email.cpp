c7ccaa43b31d7653a5627bede6b150e7b3378a94
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2023-09-20 10:07:52 +0200

    email

---
+++ email/email.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+struct eMailMsg;
+void removeSpam(vector<eMailMsg> &arr);
+vector<eMailMsg> arr;
+
+struct eMailMsg {
+    string to;
+    string from;
+    string msg;
+    string subject;
+    int date;
+    int time;
+};
+
+void removeSpam(vector<eMailMsg> &arr) {
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        eMailMsg mail = arr[i];
+            if (mail.subject.find("SPAM") == 0) {
+                arr.erase(arr.begin() + i);
+            }
+    }
+}
